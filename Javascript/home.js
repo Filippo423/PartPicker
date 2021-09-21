b7c2a3ed6b95d14406da714952e410212864cd01
AlexMolo12 <82962344+AlexMolo12@users.noreply.github.com>
2021-09-21 08:11:42 +0200

    Add files via upload

---
+++ Javascript/home.js
@@ -0,0 +1,9 @@
+function goToCpu(){
+    location.href="http://localhost/OrbuAlex/PartPicker-main/php/cpulist.php";
+}
+function goToMobo(){
+    location.href="http://localhost/OrbuAlex/PartPicker-main/php/mobolist.php";
+}
+function goToRam(){
+    location.href="http://localhost/OrbuAlex/PartPicker-main/php/ramlist.php";
+}
--- php/home.php
+++ php/home.php
@@ -12,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/header&footer.css">
     <link rel="stylesheet" href="../css/home.css">
+    <script src="../Javascript/home.js"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -40,36 +41,37 @@
     <section>
         <div class="container">
             <div class="row"><!-- apertura riga -->
-                <div class="col-sm-2" style="border-top: 1px black solid;">
-                    <p class="list_par">Component</p>
-                </div>
                 <?php
                 if(!isset($_SESSION["cpu"])){
                 ?>
-                <div class='col-sm-3' style='border-top: 1px black solid;'>
-                <button class='components_btn'><a href='cpulist.php'>aggiungi CPU</a></button>
+                <div class='d-grid gap-2 col-10 mx-auto'>
+                <button class='btn-primary btn-lg' onclick="goToCpu()">Add CPU</button>
                 </div>
             </div><!-- chiusura riga riga -->
             <?php               
             }else{
             ?>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Name</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Socket</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Memory type</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Memory frequency</p></div>
+                <div class="col-sm-2 table_up_list">
+                    <p class="list_par">Component</p>
+                </div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Name</p></div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Socket</p></div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Memory type</p></div>
+                <div class='col-sm-2 table_up_list table_end_list'><p class='list_par'>Memory frequency</p></div>
                 <div class='col-sm-2'>
-					<form action='delete.php' method='post' style='margin-top: 70px;'>
+					<form action='delete.php' method='post'>
 						<input type='hidden' name='deletepiece' value='cpu'>
-						<input type='submit' value='X'>
+						<input class="btn btn-danger" type='submit' value='X'>
 					</form>
                 </div>
             </div><!-- chiusura riga -->   
             <div class='row'><?php
                 $cpuInfo = array_values($_SESSION["cpu"]);
-                echo "<div class='col-sm-2' style='border-bottom: 1px black solid;'><p class='cont_par'>CPU</p></div>";
-                for($i=0; $i<count($cpuInfo); $i++){
-                    echo "<div class='col-sm-2' style='border-bottom: 1px black solid;'> <p class='cont_par'>".$cpuInfo[$i]."</p></div>";
+                echo "<div class='col-sm-2 table_down_list'><p class='cont_par'>CPU</p></div>";
+                for($i=0; $i<count($cpuInfo)-1; $i++){
+                    echo "<div class='col-sm-2 table_down_list'> <p class='cont_par'>".$cpuInfo[$i]."</p></div>";
                 }
+                echo "<div class='col-sm-2 table_down_list table_end_list'> <p class='cont_par'>".$cpuInfo[count($cpuInfo)-1]."</p></div>";
             }
             ?>
             </div>
@@ -80,37 +82,38 @@
     <section>
         <div class="container">
             <div class="row"><!-- apertura riga -->
-                <div class="col-sm-2" style="border-top: 1px black solid;">
-                    <p class="list_par">Component</p>
-                </div>
                 <?php
                 if(!isset($_SESSION["mobo"])){
                 ?>
-                <div class='col-sm-3' style='border-top: 1px black solid;'>
-                <button class='components_btn'><a href='mobolist.php'>aggiungi MOBO</a></button>
+                <div class='d-grid gap-2 col-10 mx-auto'>
+                <button class='btn-primary btn-lg' onclick="goToMobo()">Add Motherboard</button>
                 </div>
             </div><!-- chiusura riga -->
             <?php
             }else{
                 ?>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Name</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Socket</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Memory type</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Memory frequency</p></div>
+                <div class="col-sm-2 table_up_list">
+                    <p class="list_par">Component</p>
+                </div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Name</p></div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Socket</p></div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Memory type</p></div>
+                <div class='col-sm-2 table_up_list table_end_list'><p class='list_par'>Memory frequency</p></div>
                 <div class='col-sm-2'>
-					<form action='delete.php' method='post' style='margin-top: 70px;'>
+					<form action='delete.php' method='post'>
 						<input type='hidden' name='deletepiece' value='mobo'>
-						<input type='submit' value='X'>
+						<input class="btn btn-danger" type='submit' value='X'> 
 					</form>
                 </div>
             </div><!-- chiusura riga -->
             <?php
             echo "<div class='row'>";
             $moboInfo = array_values($_SESSION["mobo"]);
-            echo "<div class='col-sm-2' style='border-bottom: 1px black solid;'><p class='cont_par'>Mobo</p></div>";
-            for($i=0; $i<count($moboInfo); $i++){
-                echo "<div class='col-sm-2' style='border-bottom: 1px black solid;'> <p class='cont_par'>".$moboInfo[$i]."</p></div>";
+            echo "<div class='col-sm-2 table_down_list'><p class='cont_par'>Mobo</p></div>";
+            for($i=0; $i<count($moboInfo)-1; $i++){
+                echo "<div class='col-sm-2 table_down_list'><p class='cont_par'>".$moboInfo[$i]."</p></div>";
             }
+            echo "<div class='col-sm-2 table_down_list table_end_list'> <p class='cont_par'>".$moboInfo[count($moboInfo)-1]."</p></div>";
         }
         ?>
         </div>
@@ -120,36 +123,37 @@
     <section>
         <div class="container">
             <div class="row"><!-- apertura riga -->
-                <div class="col-sm-2" style="border-top: 1px black solid;">
-                    <p class="list_par">Component</p>
-                </div>
                 <?php
                 if(!isset($_SESSION["ram"])){
                 ?>
-                <div class='col-sm-3' style='border-top: 1px black solid;'>
-                    <button class='components_btn'><a href='ramlist.php'>aggiungi ram</a></button>
+                <div class='d-grid gap-2 col-10 mx-auto'>
+                    <button class='btn-primary btn-lg' onclick="goToRam()">Add RAM</button>
                 </div>
             </div><!-- chiusura riga -->
             <?php
             }else{
                 ?>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Name</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Type</p></div>
-                <div class='col-sm-2' style='border-top: 1px black solid;'><p class='list_par'>Frequency</p></div>
+                <div class="col-sm-2 table_up_list" >
+                    <p class="list_par">Component</p>
+                </div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Name</p></div>
+                <div class='col-sm-2 table_up_list'><p class='list_par'>Type</p></div>
+                <div class='col-sm-2 table_up_list table_end_list'><p class='list_par'>Frequency</p></div>
                 <div class='col-sm-2'>
-				    <form action='delete.php' method='post' style='margin-top: 70px;'>
+				    <form action='delete.php' method='post'>
 					    <input type='hidden' name='deletepiece' value='ram'>
-					    <input type='submit' value='X'>
+					    <input class="btn btn-danger"type='submit' value='X'>
 				    </form>
                 </div>
             </div><!-- chiusura riga -->
             <?php    
                 echo "<div class='row'>";
                 $ramInfo = array_values($_SESSION["ram"]);
-                echo "<div class='col-sm-2' style='border-bottom: 1px black solid;'><p class='cont_par'>RAM</p></div>";
-                for($i=0; $i<count($ramInfo); $i++){
-                    echo "<div class='col-sm-2' style='border-bottom: 1px black solid;'> <p class='cont_par'>".$ramInfo[$i]."</p></div>";
+                echo "<div class='col-sm-2 table_down_list'><p class='cont_par'>RAM</p></div>";
+                for($i=0; $i<count($ramInfo)-1; $i++){
+                    echo "<div class='col-sm-2 table_down_list'> <p class='cont_par'>".$ramInfo[$i]."</p></div>";
                 }
+                echo "<div class='col-sm-2 table_down_list table_end_list'> <p class='cont_par'>".$ramInfo[count($ramInfo)-1]."</p></div>";
             }
             ?>
         </div>
